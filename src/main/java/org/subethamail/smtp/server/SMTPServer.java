606070fccb7f538e9376e95307170fcb93c2a6e2
Valentin Popov <popov@mailarchiva.ru>
2021-07-30 13:40:36 +0300

    #44

--- src/main/java/org/subethamail/smtp/server/SMTPServer.java
+++ src/main/java/org/subethamail/smtp/server/SMTPServer.java
@@ -12,6 +12,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import javax.annotation.concurrent.GuardedBy;
 import javax.net.ssl.SSLContext;
@@ -28,6 +29,7 @@ import org.subethamail.smtp.helper.BasicMessageHandlerFactory;
 import org.subethamail.smtp.helper.BasicMessageListener;
 import org.subethamail.smtp.helper.SimpleMessageListener;
 import org.subethamail.smtp.helper.SimpleMessageListenerAdapter;
+import org.subethamail.smtp.internal.command.MailCommand;
 import org.subethamail.smtp.internal.server.AcceptAllSessionHandler;
 import org.subethamail.smtp.internal.server.CommandHandler;
 import org.subethamail.smtp.internal.server.ServerThread;
@@ -237,6 +239,8 @@ public final class SMTPServer implements SSLSocketCreator {
 
         private SessionHandler sessionHandler = AcceptAllSessionHandler.INSTANCE;
 
+        private CommandHandler commandHandler = new CommandHandler();
+
         /* No proxy handling attempt by default */
         private ProxyHandler proxyHandler = ProxyHandler.NOP;
 
@@ -348,6 +352,16 @@ public final class SMTPServer implements SSLSocketCreator {
 
         public Builder enableTLS(boolean value) {
             this.enableTLS = value;
+            return this;
+        }
+
+        /**
+         *  <p>Add filter that will be applied to address when server receive {@code MAIL FROM:} command</p>
+         * @param filter the filter will be applied to the address in MAIL FROM command
+         * @return
+         */
+        public Builder mailFromAddressFilter(Predicate<String> filter) {
+            this.commandHandler.addCommand(new MailCommand(filter));
             return this;
         }
 
@@ -591,11 +605,12 @@ public final class SMTPServer implements SSLSocketCreator {
             if (listener.isPresent()) {
                 messageHandlerFactory(new BasicMessageHandlerFactory(listener.get(), maxMessageSize));
             }
+
             return new SMTPServer(hostName, bindAddress, port, backlog, softwareName, messageHandlerFactory,
                     authenticationHandlerFactory, executorService, enableTLS, hideTLS, requireTLS, requireAuth,
                     showAuthCapabilitiesBeforeSTARTTLS, disableReceivedHeaders, maxConnections, connectionTimeoutMs,
                     maxRecipients, maxMessageSize, sessionIdFactory, sessionHandler, proxyHandler, startTlsSocketCreator,
-                    serverSocketCreator, serverThreadNameProvider);
+                    serverSocketCreator, serverThreadNameProvider, commandHandler);
         }
 
     }
@@ -608,7 +623,7 @@ public final class SMTPServer implements SSLSocketCreator {
             int maxConnections, int connectionTimeoutMs, int maxRecipients, int maxMessageSize,
             SessionIdFactory sessionIdFactory, SessionHandler sessionHandler, ProxyHandler proxyHandler,
             SSLSocketCreator startTlsSocketFactory, ServerSocketCreator serverSocketCreator,
-            Function<SMTPServer, String> serverThreadNameProvider) {
+            Function<SMTPServer, String> serverThreadNameProvider, CommandHandler commandHandler) {
         Preconditions.checkNotNull(messageHandlerFactory);
         Preconditions.checkNotNull(bindAddress);
         Preconditions.checkNotNull(executorService);
@@ -639,7 +654,7 @@ public final class SMTPServer implements SSLSocketCreator {
         this.sessionIdFactory = sessionIdFactory;
         this.sessionHandler = sessionHandler;
         this.proxyHandler = proxyHandler;
-        this.commandHandler = new CommandHandler();
+        this.commandHandler = commandHandler;
         this.serverSocketCreator = serverSocketCreator;
         this.startTlsSocketCreator = startTlsSocketFactory;
 
